a787aeed0c2f0d050e6e4b16a4e1917bde85a56c
MJunaidK <junaidcts@gmail.com>
2020-12-20 18:51:41 -0500

    Serving file statically

--- Express/app.js
+++ Express/app.js
@@ -17,6 +17,7 @@ app.use((req,res, next) => {
 */ 
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/admin', adminRoutes);
