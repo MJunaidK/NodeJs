ab7c14d382f26e013cd3b7fbc1062db1c582edfd
MJunaidK <junaidcts@gmail.com>
2020-12-19 21:29:20 -0500

    Using express

--- Express/app.js
+++ Express/app.js
@@ -1,7 +1,36 @@
-const http = require('http');
+const express = require('express');
 
-const app = require('express');
+const app = express();
 
-const server = http.createServer(app);
+const bodyParser = require('body-parser');
 
-server.listen(3000);
+/*
+app.use((req,res, next) => {
+    console.log('In the middleware');
+    next(); // Allows the request to continue to the next middleware in line.
+});
+*/ 
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/',(req,res, next) => {
+    console.log('This always run');
+    next();
+});
+
+app.use('/add-product',(req,res, next) => {
+    console.log('In another middleware');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/',(req,res, next) => {
+    console.log('In another middleware');
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(3000);
