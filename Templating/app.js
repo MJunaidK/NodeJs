8b51a4a0653fbb5723b8d2076ab44c9ffdbe6bdf
MJunaidK <junaidcts@gmail.com>
2020-12-21 21:27:14 -0500

    Adding tempating initial code

---
+++ Templating/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const bodyParser = require('body-parser');
+
+/*
+app.use((req,res, next) => {
+    console.log('In the middleware');
+    next(); // Allows the request to continue to the next middleware in line.
+});
+*/ 
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+/*
+app.use('/',(req,res, next) => {
+    console.log('This always run');
+    next();
+});
+*/
+
+app.use((req, res, next) => {
+    //res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
