e6a5357aa19a45221d2c79d28b115c6374da4162
MJunaidK <junaidcts@gmail.com>
2020-12-23 13:41:11 -0500

    Converting HTML files to PUG

--- Templating/app.js
+++ Templating/app.js
@@ -35,7 +35,8 @@ app.use('/',(req,res, next) => {
 
 app.use((req, res, next) => {
     //res.status(404).send('<h1>Page not found</h1>');
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404')
 });
 
 app.listen(3000);
