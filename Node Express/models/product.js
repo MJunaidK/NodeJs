91bbdd61b78418c016cbefd0fb7cc1dcd40a5402
MJunaidK <junaidcts@gmail.com>
2020-12-25 21:09:53 -0500

    file system read and write prodcut

--- Node Express/models/product.js
+++ Node Express/models/product.js
@@ -1,6 +1,30 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+);
+
+const getProductsFromFile = cb => {
+    
+    fs.readFile(p, (err, fileContent) => {
+        if(err) {
+           cb([]);
+        }else {
+            try{
+            console.log(JSON.parse(fileContent));
+            let products= JSON.parse(fileContent);
+            cb(products);
+            } catch(err){
+                cb([]);
+            }
+        }
+        
+    })
+}
+
 module.exports = class Product {
 
     constructor(t){
@@ -8,35 +32,15 @@ module.exports = class Product {
     }
 
     save(){
-        const p = path.join(
-            path.dirname(process.mainModule.filename),
-            'data',
-            'products.json'
-            );
-        //products.push(this);
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if(!err) {
-                products = JSON.parse(fileContent);
-            }
+        getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err);
             })
-        });
+        })
     }
 
     static fetchAll(cb){
-        const p = path.join(
-            path.dirname(process.mainModule.filename),
-            'data',
-            'products.json'
-        );
-        fs.readFileSync(p, (err, fileContent) => {
-            if(err) {
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        })
+        getProductsFromFile(cb);
     }
 }
