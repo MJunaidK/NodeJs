8c761cba72a3007b36db92da2b6f42c59c75a8a6
MJunaidK <junaidcts@gmail.com>
2020-12-27 16:57:42 -0500

    Adding a Cart Model

--- Node Express/routes/shop.js
+++ Node Express/routes/shop.js
@@ -17,7 +17,7 @@ router.get('/products/:productId', shopController.getProduct);
 
 router.get('/cart', shopController.getCart);
 
-router.post('/cart', shopController.getCart);
+router.post('/cart', shopController.postCart);
 
 router.get('/orders', shopController.getOrders);
 
