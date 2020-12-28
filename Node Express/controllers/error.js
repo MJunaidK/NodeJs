2c11a64c280a2a0838e570ebde60535f59119d0b
MJunaidK <junaidcts@gmail.com>
2020-12-28 16:47:21 -0500

    Bug fixes

--- Node Express/controllers/error.js
+++ Node Express/controllers/error.js
@@ -1,8 +1,3 @@
-const path = require('path');
-
 exports.get404 = (req, res, next) => {
-    res.status(404).render('404', {
-        pageTitle: 'Page Not found',
-        path: '/404'
-    });
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
 };
