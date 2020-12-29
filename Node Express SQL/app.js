954eb2e5a2c0eae81102a325804b5ab8aa790511
MJunaidK <junaidcts@gmail.com>
2020-12-29 16:25:17 -0500

    Fetching products from database

--- Node Express SQL/app.js
+++ Node Express SQL/app.js
@@ -19,14 +19,6 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-db.execute('SELECT * from products')
-   .then(result => {
-       console.log(result[0], result[1]);
-   })
-   .catch( err => {
-    console.log(err);
-   });
-
 /*
 app.use((req,res, next) => {
     console.log('In the middleware');
