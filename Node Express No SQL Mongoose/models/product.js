252f0e3a4531bdb0b0622f072f94a4d705cff176
MJunaidK <junaidcts@gmail.com>
2021-01-04 21:58:38 -0500

    Creating the product schema

--- Node Express No SQL Mongoose/models/product.js
+++ Node Express No SQL Mongoose/models/product.js
@@ -1,3 +1,36 @@
+const mongoose =require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const productsSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  }, 
+  imageUrl: {
+    type: String,
+    required: true
+  },
+})
+
+
+
+
+
+
+
+
+
+
+
 /*const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
