e5df0830f034fd0dfed0cf10dab866c60d96d75f
MJunaidK <junaidcts@gmail.com>
2021-01-10 13:08:02 -0500

    Implemeting an authentication flow

--- Authentication/app.js
+++ Authentication/app.js
@@ -57,7 +57,7 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    User.findOne().then(user => {
+    /*User.findOne().then(user => {
       if (!user) {
         const user = new User({
           name: 'Max',
@@ -68,7 +68,7 @@ mongoose
         });
         user.save();
       }
-    });
+    });*/
     app.listen(3000);
   })
   .catch(err => {
