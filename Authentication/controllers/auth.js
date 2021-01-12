90a29582b7ef8b009ac15d275b03a5b4350f29f5
MJunaidK <junaidcts@gmail.com>
2021-01-12 13:54:27 -0500

    Sending email

--- Authentication/controllers/auth.js
+++ Authentication/controllers/auth.js
@@ -1,6 +1,13 @@
 const bcrypt = require('bcryptjs');
-
+const nodemailer = require('nodemailer');
+const sendGridTransport = require('nodemailer-sendgrid-transport');
 const User = require('../models/user');
+
+const transporter = nodemailer.createTransport(sendGridTransport({
+  auth:{
+    api_key: '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'
+  }
+}))
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error');
@@ -82,7 +89,16 @@ exports.postSignup = (req, res, next) => {
       })  
       .then(result => {
         res.redirect('/login');
+        return transporter.sendMail({
+          to: email,
+          from: 'shop@node-complete.com',
+          subject: 'Sign up succeeded!',
+          html: '<h1> You sucessfully signed up!</h1>'
+        })
       })
+      .catch(err => {
+        console.log(err);
+      });
     })
     .catch(err => {
       console.log(err);
