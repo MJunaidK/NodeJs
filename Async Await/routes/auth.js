14210c5d194237e1a3e32a7cd164a097bbc1942d
MJunaidK <junaidcts@gmail.com>
2021-02-01 20:29:52 -0500

    Asyn await initial code

---
+++ Async Await/routes/auth.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { body } = require('express-validator/check');
+
+const User = require('../models/user');
+const authController = require('../controllers/auth');
+
+const router = express.Router();
+
+router.put('/signUp', [
+    body('email')
+        .isEmail()
+        .withMessage('Please enter a valid email.')
+        .custom((value, { req }) => {
+            return User.findOne({ email: value}).then( userDoc => {
+                if(userDoc) {
+                    return Promise.reject('E-mail address already exists!')
+                }
+            })
+        })
+        .normalizeEmail(),
+    body('password')    
+        .trim()
+        .not()
+        .isEmpty()
+    ],
+    authController.signup
+);
+
+router.post('/login', authController.login);
+
+module.exports = router; 
