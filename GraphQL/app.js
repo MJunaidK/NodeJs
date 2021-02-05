37d7ca25d5ef1cf9085cff397bc92acd3c989ece
MJunaidK <junaidcts@gmail.com>
2021-02-05 15:42:01 -0500

    Adding input validation

--- GraphQL/app.js
+++ GraphQL/app.js
@@ -59,7 +59,16 @@ app.use('/graphql',
        graphqlHTTP({
           schema: graphqlSchema,
           rootValue: graphqlResolver,
-          graphiql: true
+          graphiql: true,
+          formatError(err){
+            if(!err.originalError){
+              return err;
+            }
+            const data = err.originalError.data;
+            const message = err.message || 'An error ocurred';
+            const code = err.originalError.code || 500;
+            return { message: message, status: code, data: data};
+          }
         }))
 
 app.use((error, req, res, next) => {
